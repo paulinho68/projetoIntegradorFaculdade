967d2b417cfbcd454fe023f1fb3dc8d4ce6834f3
Paulo Vitor <paulovitor2123@gmail.com>
2021-12-11 21:43:55 -0300

    relacionamento muitos pra muitos

--- ReversoBD/Entities/AreaInvestimento.cs
+++ ReversoBD/Entities/AreaInvestimento.cs
@@ -1,14 +1,10 @@
 ﻿
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ReversoBD.Entities
 {
-    [Table("areaInvestimento")]
     public class AreaInvestimento
     {
-        [Key]
         public int Id { get; set; }
         public string Nome{ get; set; }
         public virtual ICollection<Usuario> Usuarios{ get; set; }
--- ReversoBD/Mappings/TipoInvestidorMaps.cs
+++ ReversoBD/Mappings/TipoInvestidorMaps.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using ReversoBD.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReversoBD.Mappings
 {
@@ -15,7 +10,7 @@ namespace ReversoBD.Mappings
         {
             builder.Property(x => x.Id).IsRequired().UseMySqlIdentityColumn();
             builder.Property(x => x.Nome).IsRequired().HasMaxLength(255);
-            builder.HasMany(x => x.PessoasFisicas).WithOne(x => x.TipoInvestidor).HasForeignKey(x => x.idTipoInvestidor).OnDelete(DeleteBehavior.Restrict); ;
+            builder.HasMany(x => x.PessoasFisicas).WithOne(x => x.TipoInvestidor).HasForeignKey(x => x.idTipoInvestidor).OnDelete(DeleteBehavior.Restrict);
             builder.ToTable("tipo_investidor");
         }
     }
